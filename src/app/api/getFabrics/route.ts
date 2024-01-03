6ab0688b371a3b9853a61faff8cdca0f9f207a88
Muhammed Firat Ozturk <firatdmf@gmail.com>
2024-01-03 18:06:39 +0300

    commented out the getFabrics api route

--- src/app/api/getFabrics/route.ts
+++ src/app/api/getFabrics/route.ts
@@ -1,27 +1,27 @@
-import { PrismaClient } from '@prisma/client';
-// import { NextApiRequest, NextApiResponse } from 'next';
-import { prisma } from "@/lib/prisma";
-import { NextResponse } from "next/server";
+// import { PrismaClient } from '@prisma/client';
+// // import { NextApiRequest, NextApiResponse } from 'next';
+// import { prisma } from "@/lib/prisma";
+// import { NextResponse } from "next/server";
 
-// export async function GET(request: Request){
-//   return new Response('hi')
+// // export async function GET(request: Request){
+// //   return new Response('hi')
+// // }
+
+// // export async function POST(request: Request){
+// //   return new Response('hi')
+// // }
+
+// export async function GET() {
+//   const data = {
+//     name: 'bishal',
+//     age: '27'
+//   }
+//   const fabrics = await prisma.collections.findFirst({
+//     where:{
+//       collection:'products_embroidered_sheer_curtain_fabrics',
+//     }
+//   })
+//   console.log(typeof(fabrics));
+//   // return NextResponse.json({ data })
+//   return new Response(JSON.stringify(fabrics))
 // }
-
-// export async function POST(request: Request){
-//   return new Response('hi')
-// }
-
-export async function GET() {
-  const data = {
-    name: 'bishal',
-    age: '27'
-  }
-  const fabrics = await prisma.collections.findFirst({
-    where:{
-      collection:'products_embroidered_sheer_curtain_fabrics',
-    }
-  })
-  console.log(typeof(fabrics));
-  // return NextResponse.json({ data })
-  return new Response(JSON.stringify(fabrics))
-}
