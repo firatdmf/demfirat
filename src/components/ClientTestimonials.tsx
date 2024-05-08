3e3f85ce24cdceead8886b7467e8f396d563e778
Muhammed Firat Ozturk <firatdmf@gmail.com>
2024-05-08 22:17:52 +0300

    forgot to omit the development in testimonial

--- src/components/ClientTestimonials.tsx
+++ src/components/ClientTestimonials.tsx
@@ -3,7 +3,7 @@ import classes from "./ClientTestimonials.module.css";
 function ClientTestimonials() {
   return (
     <div className={classes.ClientTestimonialsPage}>
-      <h1>Here comes the page for client testimonials</h1>
+      {/* <h1>Here comes the page for client testimonials</h1> */}
     </div>
   );
 }
