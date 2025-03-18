0eda4c64a8f0c644f1bbef22a1d518724d513086
Firat <firatdmf@gmail.com>
2025-03-18 16:12:19 +0300

    halfway done with product detail card

--- src/app/[locale]/product/curtain/[sku]/page.tsx
+++ src/app/[locale]/product/curtain/[sku]/page.tsx
@@ -1,7 +1,9 @@
 import ProductDetailCardNew from "@/components/ProductDetailCardNew"
 // import { GetServerSideProps } from 'next';
 import { prisma } from '@/lib/prisma';
-import { Product } from "@/components/ProductGridNew";
+import { Product, ProductVariant } from "@/components/ProductGridNew";
+import fs from 'fs'
+import path from 'path'
 // import { useRouter } from 'next/router'
 // interface ProductDetailCardProps{
 //   product: Product | null;
@@ -17,27 +19,57 @@ interface PageProps {
   }
 }
 
+
+
 // Params is an object that is passed from the url so you put braces around it.
 async function page({ params }: PageProps) {
+
   // const router = useRouter()
   // const {asd} = router.query
   // const {asd} = params
   const sku = params.sku
-  const product = await prisma.marketing_product.findFirst({
+  const product: Product | null = await prisma.marketing_product.findFirst({
     where: {
       // sku: sku as string,
       sku: sku,
     },
   });
+  // const productVariant: ProductVariant[] | null = await prisma.marketing_productvariant.findMany({
+  //   where: {
+  //     product_id: product?.id
+  //   }
+  // })
+  // console.log("your product variants are");
+  // console.log(productVariant);
+
+
+
+
+  let imageFiles: string[] = [];
+
+  if (product && product.sku) {
+    const productImageFolder = path.join(process.cwd(), 'public', 'image', 'product', 'curtain', product.sku);
+
+    fs.readdir(productImageFolder, (err: NodeJS.ErrnoException | null, files: string[]) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      files.forEach((file) => { imageFiles.push(file) })
+      imageFiles.push()
+
+    });
+  }
+
+  // console.log(imageFiles);
+
 
   return (
     <>
 
       {product ?
         <div>
-          <p>Hello {sku}</p>
-          <p>Your product title is: {product?.title}</p>
-          <ProductDetailCardNew product={product} />
+          <ProductDetailCardNew product={product} imageFiles={imageFiles} />
         </div> : `No product found with sku: ${sku}`}
 
 
--- src/components/ProductDetailCardNew.tsx
+++ src/components/ProductDetailCardNew.tsx
@@ -6,27 +6,32 @@ import { prisma } from "@/lib/prisma";
 import { usePathname } from 'next/navigation'
 import { Decimal } from '@prisma/client/runtime/library';
 import { Product } from '@/components/ProductGridNew';
+// import "@/lib/fotorama/fotorama.js"
+// import "@/lib/fotorama/fotorama.css"
+// type ProductDetailCardNewProps {
+//   product: Product
+// }
 
-
-interface ProductDetailCardNewProps {
-  product:Product
+type ProductDetailCardNewPageProps = {
+  product: Product,
+  imageFiles: string[]
 }
 
 
 
 
-
-async function ProductDetailCardNew({product}:ProductDetailCardNewProps) {
+// this product passed down from app/[locale]/product/........./[sku]/page.tsx
+function ProductDetailCardNew({ product, imageFiles }: ProductDetailCardNewPageProps) {
   // const pathname = usePathname() // /products/curtains/ready-made/RK24562GW
   // const sku = pathname.split('/')[4]
   // const [product, setProduct] = useState<Product | null>(null);
   // useEffect(()=>{
   //   async function fetchProduct(){
   //     console.log('now sending a fetch request!!!!!!!!!');
-      
+
   //     const response = await fetch(`http://localhost:3000/api/get_product?sku=${sku}`)
   //     console.log(`your response is: ${response}`);
-      
+
   //     const fetchedProduct: Product = await response.json();
   //     setProduct(fetchedProduct)
   //   }
@@ -34,13 +39,73 @@ async function ProductDetailCardNew({product}:ProductDetailCardNewProps) {
 
   // },[sku]);
 
+  const [SelectedThumbIndex, setSelectedThumbIndex] = useState<number | null>(0);
+  const [imageLoaded, setImageLoaded] = useState<boolean>(false);
+  const selectThumb = (index: number) => {
+    setSelectedThumbIndex(index);
+  };
+
+  // const handleImageLoad = () => {
+  //   setImageLoaded(false); // Set image loaded state to true when image has loaded
+  //   setImageLoaded(true); // Set image loaded state to true when image has loaded
+  // };
+
+
+
   if (!product) {
     return <div>Loading...</div>;
   }
   
+
+
   return (
     <div className={classes.ProductDetailCardNew}>
-      <p>Your object found is: {product.title}</p>
+      <div className={classes.detailCardContainer}>
+        <div className={classes.productMedia}>
+          <div className={classes.thumbs}>
+            {/* <div className={` ${classes.thumb} ${classes.thumb_selected}`}></div> */}
+            {imageFiles.map((image, index) => {
+              return (
+                <div
+                  className={`${classes.thumb} ${SelectedThumbIndex === index ? classes.thumb_selected : ''}`}
+                  key={index}
+                  onClick={() => selectThumb(index)}>
+
+                  <div className={classes.img}>
+                    <img
+                      src={`/image/product/curtain/${product.sku}/${image}`}
+                      alt=""
+                      // onLoad={handleImageLoad}
+                    />
+                  </div>
+
+                </div>
+              )
+            })}
+          </div>
+          <div className={classes.gallery}>
+            <div className={imageLoaded ? ` ${classes.img} ${classes.loaded}` : `${classes.img}`}>
+
+              <img
+                src={`/image/product/curtain/${product.sku}/${imageFiles[SelectedThumbIndex ?? 0]}`}
+                alt=""
+                // onLoad={handleImageLoad}
+                // className={imageLoaded ? `${classes.loaded}` : ''}
+              />
+            </div>
+
+
+            {/* <img src="/media/products/embroidered_sheer_curtain_fabrics/1798T_G31.webp" alt="" /> */}
+          </div>
+
+        </div>
+        <div className={classes.productHero}>
+          <h2>{product.title}</h2>
+          <p>{product.sku}</p>
+          {product.price ? <p>{String(product.price)}</p> : <></>}
+
+        </div>
+      </div>
     </div>
   )
 }
