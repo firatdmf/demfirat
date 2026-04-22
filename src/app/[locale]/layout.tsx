e2aeb9fd1fbfb01573efeb5816e528f9d4e1888e
enesarmagan7 <enes34.point@gmail.com>
2026-04-22 11:27:41 +0300

    .

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -77,19 +77,16 @@ export default async function RootLayout(props: LayoutProps<'/[locale]'>) {
         {/* Meta Pixel Code */}
         <Script id="meta-pixel" strategy="lazyOnload">
           {`
-            try {
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.onerror=function(){/* suppress ad-blocker errors */};
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '3893578660935588');
-              fbq('track', 'PageView');
-            } catch(e) { /* fbq blocked by client — silent */ }
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '3893578660935588');
+            fbq('track', 'PageView');
           `}
         </Script>
         <noscript>
